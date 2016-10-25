56444308b1920814b71b87dee16b4bb02af63300
Duncan Marsh <dwolfm@gmail.com>
2016-10-25 08:50:19 -0700

    modified require.context naming for view controllers

--- app/entry.js
+++ app/entry.js
@@ -29,21 +29,21 @@ context.keys().forEach( path => {
 // load view controllers
 context = require.context('./view/', true, /.js$/);
 context.keys().forEach( key => {
-  let name = pascalcase(`${path.basename(key, '.js')}Controller`);
+  let name = pascalcase(path.basename(key, '.js'));
   demoApp.controller(name, context(key));
 });
 
 // load services 
 context = require.context('./service/', true, /.js$/);
 context.keys().forEach( key => {
-  let name = camelcase(`${path.basename(key, '.js')}`);
+  let name = camelcase(path.basename(key, '.js'));
   demoApp.service(name, context(key));
 });
 
 // load components 
 context = require.context('./component/', true, /.js$/);
 context.keys().forEach( key => {
-  let name = camelcase(`${path.basename(key, '.js')}`);
+  let name = camelcase(path.basename(key, '.js'));
   console.log('loading component', name);
   demoApp.component(name, context(key));
 });
